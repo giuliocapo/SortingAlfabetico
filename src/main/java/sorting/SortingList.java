40af9d725df8623fdc8a2dea967475c3f964de1c
giuliocapo <caporrogiulio@gmail.com>
2022-06-20 10:45:22 +0200

    Sort Working

--- src/main/java/sorting/SortingList.java
+++ src/main/java/sorting/SortingList.java
@@ -8,7 +8,19 @@ public class SortingList {
 
         List<String> stringList = Arrays.asList("Ispw", "Davide", "Giulio", "Guglielmo", "Francesco", "Sonia");
         List<String> sortedList = stringList.stream().sorted().collect(Collectors.toList());
-        sortedList.add(2,"fra");
+        sortedList.add(0,"startFromHere");
+        //qui si stampa a schermo la prima lista sorted
+        //gave you the list sorted
+
+        //if u want to add others
+
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        List<String> sortedList1 = stringList.stream().sorted().collect(Collectors.toList());
+        sortedList1.add(1,string);
+        sortedList1.add(0,"startFromHere");
+        //qui si stampa a schermo la seconda lista con l'aggiunta da terminale di una stringa
+
 
 
     }
