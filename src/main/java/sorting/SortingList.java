cfa927a13eaad30e7b94a1bb5aeaa969f9a2abe8
giuliocapo <caporrogiulio@gmail.com>
2022-06-20 10:31:05 +0200

    Sort Working

--- src/main/java/sorting/SortingList.java
+++ src/main/java/sorting/SortingList.java
@@ -1,15 +1,15 @@
 package sorting;
-
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.*;
 
 public class SortingList {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         List<String> stringList = Arrays.asList("Ispw", "Davide", "Giulio", "Guglielmo", "Francesco", "Sonia");
         List<String> sortedList = stringList.stream().sorted().collect(Collectors.toList());
         sortedList.add(2,"fra");
+
 
     }
 }
